00afd6f505ca34cdb7d3c2dd1e9ae1beba28c8a0
zodiacaca <121059464@qq.com>
2022-01-27 05:46:18 +0800

    m

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ browser.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   location.reload();
 });
 
+Number.prototype.clamp = function(min, max) {
+  return Math.min(Math.max(this, min), max)
+}
+
+const html = document.getElementsByTagName('html').item(0)
 const body = document.getElementsByTagName('body').item(0)
 const scroll = document.createElement("div")
 scroll.setAttribute("id", "evenscroll")
@@ -21,6 +26,7 @@ const pos = {
   'x': 0,
   'y': 0
 }
+let isMDown = false
 slide.onmousedown = dragMouseDown
 
 function dragMouseDown(e) {
@@ -30,17 +36,36 @@ function dragMouseDown(e) {
   document.addEventListener("mouseup", stopDrag)
 
   pos.y = e.clientY
+  isMDown = true
 }
 
 function dragMouseMove(e) {
   e.preventDefault()
 
   const ncrmnt = e.clientY - pos.y
-  slide.style.top = (slide.offsetTop + ncrmnt) + "px"
+  const top = (slide.offsetTop + ncrmnt).clamp(0, rail.offsetHeight - slide.offsetHeight)
+  slide.style.top = top + "px"
+  html.scrollTop = html.scrollTop + ncrmnt * 2.5
   pos.y = e.clientY
 }
 
 function stopDrag() {
+  isMDown = false
   document.removeEventListener("mousemove", dragMouseMove)
   document.removeEventListener("mouseup", stopDrag)
+}
+
+// ticks
+function step(timestamp) {
+  if (!isMDown) {
+    const pos = lerp(0.5, slide.offsetTop, rail.offsetHeight / 2 - slide.offsetHeight / 2)
+    slide.style.top = pos + "px"
+  }
+
+  window.requestAnimationFrame(step);
+}
+window.requestAnimationFrame(step)
+
+function lerp(t, from, to) {
+  return from + (to - from) * t
 }
